e7721a4f78f73e554cacfea6f44331b6c83fb985
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2021-11-05 15:03:15 +0600

    uri changed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const app = express()
 const port = 5000;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qu1uq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 console.log(uri);
 
